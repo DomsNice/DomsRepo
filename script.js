f464b00521cbd754384dc146b13eae968699216c
DomsNice <nice.doms@gmail.com>
2025-09-30 10:51:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const client = new Paho.Client(
         "ad13c9d3eb1a4c53b4c7cffe6d4e3fad.s1.eu.hivemq.cloud",
-        Number(8883),
+        Number(8884),
         "/mqtt",
         clientID
     );
@@ -153,6 +153,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
 
 
 
